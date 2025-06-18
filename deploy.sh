67ff3290174547f1d20f771443613b8f9347ffc9
JuanNaCl <jgarciaot@unal.edu.co>
2025-06-17 20:40:49 -0500

    MULTIPLE SERVERS

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 BRANCH=$1
 
 if [ "$BRANCH" == "main" ]; then
-    DEPLOY_PATH = "/home/azureuser/clase_3"
+    DEPLOY_PATH="/home/azureuser/clase_3"
 else
-    DEPLOY_PATH = "/home/azureuser/stage"
+    DEPLOY_PATH="/home/azureuser/stage"
 fi
 
 ssh -o StrictHostKeyChecking=no azureuser@128.85.34.22 <<HTML
