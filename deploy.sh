b03b343796b92084b84d20729ab8da515b6a2ba4
JuanNaCl <jgarciaot@unal.edu.co>
2025-06-17 20:36:56 -0500

    MULTIPLE SERVERS

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
-ssh -o StrictHostKeyChecking=no azureuser@128.85.34.22 <<HTML
- cd /home/azureuser/clase_3
+BRANCH=$1
 
- git pull --rebase origin main
+if [ "$BRANCH" == "main"]; then
+    DEPLOY_PATH = "/home/azureuser/clase_3"
+else
+    DEPLOY_PATH = "/home/azureuser/stage"
+
+
+ssh -o StrictHostKeyChecking=no azureuser@128.85.34.22 <<HTML
+ cd $DEPLOY_PATH    
+
+ git pull --rebase origin $BRANCH
 HTML
